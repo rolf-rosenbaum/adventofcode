3f8cec1b7a5a900779667e035ffd243cf61fffa2
rolf-rosenbaum <rolf.rosenbaum@gmail.com>
2019-12-11 17:06:49 +0100

    Add files via upload

---
+++ StationLocationOptimizer.kt
@@ -0,0 +1,113 @@
+package day9
+
+import kotlin.math.sign
+
+fun main(args: Array<String>) {
+    val optimizer = StationLocationOptimizer("map.txt")
+
+    println("Best location is ${optimizer.findBestAsteroid()}")
+}
+
+class StationLocationOptimizer(private val fileName: String) {
+
+
+    val asteroidMap = readInputData()
+        get() = field
+
+    private fun readInputData(): Set<Asteroid> {
+        val asteroidMap = mutableSetOf<Asteroid>()
+
+        javaClass.classLoader.getResourceAsStream(fileName)
+            .reader()
+            .readText()
+            .split("\n")
+            .mapIndexed { y, line ->
+                line.mapIndexed { x, char ->
+                    if (char == '#') {
+                        asteroidMap.add(Asteroid(x, y))
+                    }
+                }
+            }
+        return asteroidMap
+    }
+
+    fun countVisibleAsteroidsFor(asteroid: Asteroid): Asteroid {
+        if (!asteroidMap.contains(asteroid)) {
+            return asteroid
+        }
+
+        var result = asteroidMap.size - 1
+
+        asteroidMap.minusElement(asteroid).map { other ->
+
+            val sightIsBlockedBetween = asteroidMap.any { it.isInLineOfSightBetween(asteroid, other) }
+
+            if (sightIsBlockedBetween)
+                result--
+        }
+
+        asteroid.visibleCounter = result
+        return asteroid
+    }
+
+    fun findBestAsteroid(): Asteroid {
+        return asteroidMap.map {
+            countVisibleAsteroidsFor(it)
+        }.maxBy {
+            it.visibleCounter
+        }!!
+    }
+}
+
+data class Asteroid(val x: Int, val y: Int, var visibleCounter: Int = 0) {
+
+    operator fun plus(v: Vector): Asteroid = Asteroid(x + v.x, y + v.y)
+
+    operator fun minus(c: Asteroid): Vector = Vector(x - c.x, y - c.y)
+
+    private fun distanceTo(other: Asteroid): Int {
+        return (x - other.x) + (y - other.y)
+    }
+
+    private fun isInDifferentDirection(asteroid: Asteroid, other: Asteroid): Boolean {
+        return (x - asteroid.x).sign != (x-other.x).sign ||
+                (y - asteroid.y).sign != (y-other.y).sign
+    }
+
+    fun isInLineOfSightBetween(a: Asteroid, b: Asteroid): Boolean {
+        if (x == 0 && y == 0) {
+            return false
+        }
+        if (a == b || this == a || this == b) {
+            return false
+        }
+
+
+        val cb = this - b
+        val ac = this - a
+
+        return cb.isDependant(ac) && isInDifferentDirection(a, b)
+    }
+}
+
+data class Vector(val x: Int, val y: Int) {
+    fun isDependant(other: Vector): Boolean {
+
+        if (x == 0) {
+            return other.x == 0
+        }
+        if (y == 0) {
+            return other.y == 0
+        }
+        return x / gcd(x,y) == other.x / gcd(other.x, other.y) && y / gcd(x,y) == other.y / gcd(other.x, other.y)
+
+    }
+
+}
+
+fun gcd(a: Int, b: Int): Int {
+    if (b == 0) {
+        return a
+    }
+    return gcd(b, a % b)
+}
