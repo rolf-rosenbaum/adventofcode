e94c2c0a430f486d6a724a83cef6c1fa89d4694c
rolf <rolf.rosenbaum@gmail.com>
2019-12-22 19:33:37 +0100

    day 6 part one

--- src/main/kotlin/day6/Day6.kt
+++ src/main/kotlin/day6/Day6.kt
@@ -3,6 +3,7 @@ package day6
 fun main(args: Array<String>) {
     val orbitCalculator = OrbitCalculator("orbits.txt")
     println(orbitCalculator.partOne())
+    println(orbitCalculator.partTwo())
 }
 
 class OrbitCalculator(private val fileName: String) {
@@ -33,7 +34,7 @@ class OrbitCalculator(private val fileName: String) {
         return centerPlanets.first { !orbiters.any { orbiter -> orbiter.first == it } }
     }
 
-    fun distanceToCenter(orbiter: Pair<String, String>): Int {
+    private fun distanceToCenter(orbiter: Pair<String, String>): Int {
 
         if (orbiter.second == center) {
             return 1
@@ -41,9 +42,52 @@ class OrbitCalculator(private val fileName: String) {
         return 1 + distanceToCenter(orbiters.first { it.first == orbiter.second })
     }
 
+    private fun orbitsBetween(planet: String, orbiter: Pair<String, String>): Int {
+
+        if (orbiter.second == planet) {
+            return 0
+        }
+        return 1 + orbitsBetween(planet, orbiters.first { it.first == orbiter.second })
+    }
+
     fun totalOrbits(): Int = orbiters.sumBy { distanceToCenter(it) }
 
-    fun partOne() : String {
+    fun pathToCenterFrom(planet: Pair<String, String>, pathSoFar: MutableList<String> = mutableListOf()): List<String> {
+        if (planet.second == center) {
+            return pathSoFar
+        }
+        pathSoFar.add(planet.second)
+
+        return pathToCenterFrom(orbiters.first { it.first == planet.second }, pathSoFar)
+    }
+
+    fun closestCommonNode(planetA: String, planetB: String): String {
+        val pairA = orbiters.first { it.first == planetA }
+        val pairB = orbiters.first { it.first == planetB }
+
+        val commonNodes = pathToCenterFrom(pairA).intersect(pathToCenterFrom(pairB))
+
+        return commonNodes.maxBy { name ->
+            distanceToCenter(orbiters.first {
+                it.second == name
+            })
+        }!!
+    }
+
+    fun partOne(): String {
         return "total orbits: ${totalOrbits()}"
+    }
+
+    fun partTwo(): String {
+        return "orbital transfers: ${orbitalTransfers("YOU", "SAN")}"
+    }
+
+    fun orbitalTransfers(planetA: String, planetB: String): Int {
+        val pairA = orbiters.first { it.first == planetA }
+        val pairB = orbiters.first { it.first == planetB }
+
+        val closestCommonNode = closestCommonNode(planetA, planetB)
+
+        return orbitsBetween(closestCommonNode, pairA) + orbitsBetween(closestCommonNode, pairB)
     }
 }
--- src/test/kotlin/day6/Day6Test.kt
+++ src/test/kotlin/day6/Day6Test.kt
@@ -13,8 +13,29 @@ class Day6Test {
     }
 
     @Test
-    internal fun distance() {
+    internal fun totalOrbits() {
         val calculator = OrbitCalculator("orbits.txt")
         assertThat(calculator.totalOrbits()).isEqualTo(42)
     }
+
+    @Test
+    internal fun pathToCenter() {
+        val calculator = OrbitCalculator("orbits.txt")
+        assertThat(calculator.pathToCenterFrom("C" to "B")).isEqualTo(listOf("B"))
+        assertThat(calculator.pathToCenterFrom("I" to "D")).isEqualTo(listOf("D", "C", "B"))
+    }
+
+    @Test
+    internal fun closestCommonNode() {
+        val calculator = OrbitCalculator("orbits.txt")
+        assertThat(calculator.closestCommonNode("J", "I")).isEqualTo("D")
+    }
+
+    @Test
+    internal fun `orbital transfers from YOU to SAN`() {
+        val calculator = OrbitCalculator("orbits_full.txt")
+        assertThat(calculator.closestCommonNode("YOU", "SAN")).isEqualTo("D")
+        assertThat(calculator.orbitalTransfers("YOU", "SAN")).isEqualTo(4)
+    }
+
 }
