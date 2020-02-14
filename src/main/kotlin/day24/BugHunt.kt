b4df035c31f96d01d1d2ff5984c44163c9af74dd
Rolf Rosenbaum <rolf.rosenbaum@rewe-digital.com>
2020-02-14 11:31:41 +0100

    Add layers coding dojo 14.02.2020

--- src/main/kotlin/day24/BugHunt.kt
+++ src/main/kotlin/day24/BugHunt.kt
@@ -2,14 +2,17 @@ package day24
 
 import kotlin.math.pow
 
+
 fun main() {
 
 
-    val world = World("eris.txt")
+//    val world = World("eris.txt")
+//   world.generations()
 
-    world.generations()
+    val testWorld = World("test.txt")
+    testWorld.generations2()
+
 }
-
 
 class World(private val fileName: String) {
 
@@ -25,28 +28,59 @@ class World(private val fileName: String) {
 
     private fun readInput() {
         javaClass.classLoader.getResourceAsStream(fileName)
-                .reader()
-                .readLines().mapIndexed { y, line ->
-                    for (x in line.indices) {
-                        if (line[x] == '#') {
-                            livingCells.add(Cell(x, y))
-                        }
+            .reader()
+            .readLines().mapIndexed { y, line ->
+                for (x in line.indices) {
+                    if (line[x] == '#') {
+                        livingCells.add(Cell(x, y))
                     }
                 }
+            }
     }
 
     private fun nextGeneration(): Set<Cell> {
         val nextGeneration = mutableSetOf<Cell>()
-        for (x in 0..4) {
-            for (y in 0..4) {
-                val cell = Cell(x, y)
-                if (cell.isAlive()) {
-                    if (cell.countNeighbours() == 1) {
-                        nextGeneration.add(cell)
+
+        val minLayer = livingCells.minBy { it.layer }?.layer ?: 0
+        val maxLayer = livingCells.maxBy { it.layer }?.layer ?: 0
+
+        for (layer in minLayer..maxLayer) {
+            for (x in 0..4) {
+                for (y in 0..4) {
+                    val cell = Cell(x, y, layer)
+                    val cellInner = Cell(x, y, layer - 1)
+                    val cellOuter = Cell(x, y, layer + 1)
+
+                    val neighbours = cell.countNeighbours()
+                    if (cell.isAlive()) {
+                        if (neighbours == 1) {
+                            nextGeneration.add(cell)
+                        }
+                    } else {
+                        if (neighbours in 1..2) {
+                            nextGeneration.add(cell)
+                        }
                     }
-                } else {
-                    if (cell.countNeighbours() in 1..2) {
-                        nextGeneration.add(cell)
+
+                    val neighboursInner = cellInner.countNeighbours()
+                    if (cell.isAlive()) {
+                        if (neighboursInner == 1) {
+                            nextGeneration.add(cellInner)
+                        }
+                    } else {
+                        if (neighboursInner in 1..2) {
+                            nextGeneration.add(cellInner)
+                        }
+                    }
+                    val neighboursOuter = cellOuter.countNeighbours()
+                    if (cell.isAlive()) {
+                        if (neighboursOuter == 1) {
+                            nextGeneration.add(cellOuter)
+                        }
+                    } else {
+                        if (neighboursOuter in 1..2) {
+                            nextGeneration.add(cellOuter)
+                        }
                     }
                 }
             }
@@ -55,19 +89,127 @@ class World(private val fileName: String) {
         return nextGeneration.cutEdges()
     }
 
+
+    private fun nextGenerationInner(layer: Int): MutableSet<Cell> {
+        val next = mutableSetOf<Cell>()
+        val cell = Cell(2, 1, layer)
+
+
+        return next
+    }
+
+    private fun nextGenerationOuter(layer: Int): MutableSet<Cell> {
+        val next = mutableSetOf<Cell>()
+
+        return next
+    }
+
     private fun Cell.isAlive() = livingCells.contains(this)
 
     private fun Set<Cell>.cutEdges(): Set<Cell> = this.filter {
         it.x in 0..4 && it.y in 0..4
     }.toSet()
 
+    private fun Cell.countNeighboursForOuter(): Int {
+        return 0
+    }
+
+    private fun Cell.countNeighboursForInner(): Int {
+        return 0
+    }
+
     private fun Cell.countNeighbours(): Int {
-        return livingCells.intersect(listOf(
-                Cell(x, y + 1),
-                Cell(x, y - 1),
-                Cell(x - 1, y),
-                Cell(x + 1, y)
-        )).size
+
+
+        val specialCells = listOf(Cell(2, 1, layer), Cell(1, 2, layer), Cell(2, 3, layer), Cell(3, 2, layer))
+        var count = 0
+        count += countInner() + countOuter()
+
+        count += livingCells.intersect(
+            listOf(
+                Cell(x, y + 1, layer),
+                Cell(x, y - 1, layer),
+                Cell(x - 1, y, layer),
+                Cell(x + 1, y, layer)
+            )
+                .minus(Cell(2, 2, layer))
+        ).size
+
+        return count
+
+    }
+
+    private fun Cell.countOuter(): Int {
+        var count = 0
+        when {
+            y == 0 -> {
+                count += livingCells.intersect(listOf(Cell(2, 1, layer - 1))).size
+
+            }
+            y == 4 -> {
+                count += livingCells.intersect(listOf(Cell(2, 3, layer - 1))).size
+
+            }
+            x == 0 -> {
+                count += livingCells.intersect(listOf(Cell(1, 2, layer - 1))).size
+
+            }
+            x == 4 -> {
+                count += livingCells.intersect(listOf(Cell(3, 2, layer - 1))).size
+            }
+        }
+        return count
+    }
+
+
+    private fun Cell.countInner(): Int {
+        var count = 0
+        if (x == 2 && y == 1) {
+            count += livingCells.intersect(
+                listOf(
+                    Cell(0, 0, layer + 1),
+                    Cell(1, 0, layer + 1),
+                    Cell(2, 0, layer + 1),
+                    Cell(3, 0, layer + 1)
+                )
+            ).size
+        } else if (x == 1 && y == 2) {
+            count += livingCells.intersect(
+                listOf(
+                    Cell(0, 0, layer + 1),
+                    Cell(0, 1, layer + 1),
+                    Cell(0, 2, layer + 1),
+                    Cell(0, 3, layer + 1)
+                )
+            ).size
+        } else if (x == 2 && y == 3) {
+            count += livingCells.intersect(
+                listOf(
+                    Cell(0, 4, layer + 1),
+                    Cell(1, 4, layer + 1),
+                    Cell(2, 4, layer + 1),
+                    Cell(3, 4, layer + 1)
+                )
+            ).size
+        } else if (x == 3 && y == 2) {
+            count += livingCells.intersect(
+                listOf(
+                    Cell(4, 0, layer + 1),
+                    Cell(4, 1, layer + 1),
+                    Cell(4, 2, layer + 1),
+                    Cell(4, 3, layer + 1)
+                )
+            ).size
+        }
+        return count
+    }
+
+    fun generations2() {
+        for (i in 1..10) {
+            livingCells = nextGeneration().toMutableSet()
+        }
+        println("number of bugs: ${livingCells.size}")
+        println("layers: ${livingCells.minBy { it.layer }?.layer} to ${livingCells.maxBy { it.layer }?.layer}")
     }
 
     fun generations() {
@@ -99,21 +241,13 @@ class World(private val fileName: String) {
         println()
     }
 
-    fun bioDiversity(layout: Set<Cell>): Long {
-        val result = mutableListOf<Long>()
+    private fun bioDiversity(layout: Set<Cell>): Long {
 
-        for (x in 0..4) {
-            for (y in 0..4) {
-                if (Cell(x, y).isAlive()) {
-                    val tileNumber = (y * 5) + x
-                    result.add(2.0.pow(tileNumber.toDouble()).toLong())
-                }
-            }
+        return layout.fold(0) { sum: Long, cell ->
+            val tileNumber = (cell.y * 5) + cell.x
+            sum + 2.0.pow(tileNumber.toDouble()).toLong()
         }
-        return result.sum()
     }
 }
 
-
-data class Cell(val x: Int, val y: Int)
-
+data class Cell(val x: Int, val y: Int, val layer: Int = 0)
