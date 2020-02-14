d3ab5e52559974b8dd119816a485d560530ec874
Rolf Rosenbaum <rolf.rosenbaum@rewe-digital.com>
2020-02-14 16:22:26 +0100

    Day 24 solution part 2

--- src/main/kotlin/day24/BugHunt.kt
+++ src/main/kotlin/day24/BugHunt.kt
@@ -6,11 +6,10 @@ import kotlin.math.pow
 fun main() {
 
 
-//    val world = World("eris.txt")
-//   world.generations()
+    val world = World("eris.txt")
 
-    val testWorld = World("test.txt")
-    testWorld.generations2()
+    world.generations2(200)
+
 
 }
 
@@ -41,15 +40,26 @@ class World(private val fileName: String) {
     private fun nextGeneration(): Set<Cell> {
         val nextGeneration = mutableSetOf<Cell>()
 
-        val minLayer = livingCells.minBy { it.layer }?.layer ?: 0
-        val maxLayer = livingCells.maxBy { it.layer }?.layer ?: 0
-
-        for (layer in minLayer..maxLayer) {
+        for (layer in livingCells.minLayer()..livingCells.maxLayer()) {
             for (x in 0..4) {
                 for (y in 0..4) {
+                    if (x == 2 && y == 2) {
+                        continue
+                    }
+
                     val cell = Cell(x, y, layer)
+
                     val cellInner = Cell(x, y, layer - 1)
-                    val cellOuter = Cell(x, y, layer + 1)
+                    val neighboursInner = cellInner.countNeighbours()
+                    if (cellInner.isAlive()) {
+                        if (neighboursInner == 1) {
+                            nextGeneration.add(cellInner)
+                        }
+                    } else {
+                        if (neighboursInner in 1..2) {
+                            nextGeneration.add(cellInner)
+                        }
+                    }
 
                     val neighbours = cell.countNeighbours()
                     if (cell.isAlive()) {
@@ -62,18 +72,9 @@ class World(private val fileName: String) {
                         }
                     }
 
-                    val neighboursInner = cellInner.countNeighbours()
-                    if (cell.isAlive()) {
-                        if (neighboursInner == 1) {
-                            nextGeneration.add(cellInner)
-                        }
-                    } else {
-                        if (neighboursInner in 1..2) {
-                            nextGeneration.add(cellInner)
-                        }
-                    }
+                    val cellOuter = Cell(x, y, layer + 1)
                     val neighboursOuter = cellOuter.countNeighbours()
-                    if (cell.isAlive()) {
+                    if (cellOuter.isAlive()) {
                         if (neighboursOuter == 1) {
                             nextGeneration.add(cellOuter)
                         }
@@ -87,21 +88,6 @@ class World(private val fileName: String) {
         }
         generations.add(livingCells)
         return nextGeneration.cutEdges()
-    }
-
-
-    private fun nextGenerationInner(layer: Int): MutableSet<Cell> {
-        val next = mutableSetOf<Cell>()
-        val cell = Cell(2, 1, layer)
-
-
-        return next
-    }
-
-    private fun nextGenerationOuter(layer: Int): MutableSet<Cell> {
-        val next = mutableSetOf<Cell>()
-
-        return next
     }
 
     private fun Cell.isAlive() = livingCells.contains(this)
@@ -132,8 +118,8 @@ class World(private val fileName: String) {
                 Cell(x - 1, y, layer),
                 Cell(x + 1, y, layer)
             )
-                .minus(Cell(2, 2, layer))
-        ).size
+        ).minus(Cell(2, 2, layer))
+            .size
 
         return count
 
@@ -141,73 +127,91 @@ class World(private val fileName: String) {
 
     private fun Cell.countOuter(): Int {
         var count = 0
-        when {
-            y == 0 -> {
-                count += livingCells.intersect(listOf(Cell(2, 1, layer - 1))).size
+        if (y == 0) {
+            count += livingCells.intersect(listOf(Cell(2, 1, layer - 1))).size
 
-            }
-            y == 4 -> {
+        } else
+            if (y == 4) {
                 count += livingCells.intersect(listOf(Cell(2, 3, layer - 1))).size
 
             }
-            x == 0 -> {
-                count += livingCells.intersect(listOf(Cell(1, 2, layer - 1))).size
+        if (x == 0) {
+            count += livingCells.intersect(listOf(Cell(1, 2, layer - 1))).size
 
-            }
-            x == 4 -> {
+        } else
+            if (x == 4) {
                 count += livingCells.intersect(listOf(Cell(3, 2, layer - 1))).size
             }
-        }
         return count
     }
 
 
     private fun Cell.countInner(): Int {
-        var count = 0
+        var neighbours = mutableSetOf<Cell>()
+
         if (x == 2 && y == 1) {
-            count += livingCells.intersect(
-                listOf(
-                    Cell(0, 0, layer + 1),
-                    Cell(1, 0, layer + 1),
-                    Cell(2, 0, layer + 1),
-                    Cell(3, 0, layer + 1)
+            neighbours.addAll(
+                livingCells.intersect(
+                    listOf(
+                        Cell(0, 0, layer + 1),
+                        Cell(1, 0, layer + 1),
+                        Cell(2, 0, layer + 1),
+                        Cell(3, 0, layer + 1),
+                        Cell(4, 0, layer + 1)
+                    )
                 )
-            ).size
+            )
         } else if (x == 1 && y == 2) {
-            count += livingCells.intersect(
-                listOf(
-                    Cell(0, 0, layer + 1),
-                    Cell(0, 1, layer + 1),
-                    Cell(0, 2, layer + 1),
-                    Cell(0, 3, layer + 1)
+            neighbours.addAll(
+                livingCells.intersect(
+                    listOf(
+                        Cell(0, 0, layer + 1),
+                        Cell(0, 1, layer + 1),
+                        Cell(0, 2, layer + 1),
+                        Cell(0, 3, layer + 1),
+                        Cell(0, 4, layer + 1)
+                    )
                 )
-            ).size
+            )
         } else if (x == 2 && y == 3) {
-            count += livingCells.intersect(
-                listOf(
-                    Cell(0, 4, layer + 1),
-                    Cell(1, 4, layer + 1),
-                    Cell(2, 4, layer + 1),
-                    Cell(3, 4, layer + 1)
+            neighbours.addAll(
+                livingCells.intersect(
+                    listOf(
+                        Cell(0, 4, layer + 1),
+                        Cell(1, 4, layer + 1),
+                        Cell(2, 4, layer + 1),
+                        Cell(3, 4, layer + 1),
+                        Cell(4, 4, layer + 1)
+                    )
                 )
-            ).size
+            )
         } else if (x == 3 && y == 2) {
-            count += livingCells.intersect(
-                listOf(
-                    Cell(4, 0, layer + 1),
-                    Cell(4, 1, layer + 1),
-                    Cell(4, 2, layer + 1),
-                    Cell(4, 3, layer + 1)
+            neighbours.addAll(
+                livingCells.intersect(
+                    listOf(
+                        Cell(4, 0, layer + 1),
+                        Cell(4, 1, layer + 1),
+                        Cell(4, 2, layer + 1),
+                        Cell(4, 3, layer + 1),
+                        Cell(4, 4, layer + 1)
+                    )
                 )
-            ).size
+            )
         }
-        return count
+        return livingCells.intersect(neighbours).size
+
     }
 
-    fun generations2() {
-        for (i in 1..10) {
+    fun generations2(gen: Int) {
+
+        prettyPrint(livingCells)
+        repeat(gen) {
             livingCells = nextGeneration().toMutableSet()
+            println("generation: $it" )
+//            prettyPrint(livingCells)
+//            println()
         }
+
         println("number of bugs: ${livingCells.size}")
         println("layers: ${livingCells.minBy { it.layer }?.layer} to ${livingCells.maxBy { it.layer }?.layer}")
     }
@@ -231,14 +235,17 @@ class World(private val fileName: String) {
     }
 
     private fun prettyPrint(layout: Set<Cell>) {
-        for (y in 0..4) {
-            var line = ""
-            for (x in 0..4) {
-                line += if (layout.contains(Cell(x, y))) "#" else "."
+        for (layer in layout.minLayer()..layout.maxLayer()) {
+            println("Layer: $layer")
+            for (y in 0..4) {
+                var line = ""
+                for (x in 0..4) {
+                    line += if (layout.contains(Cell(x, y, layer))) "#" else "."
+                }
+                println(line)
             }
-            println(line)
+            println()
         }
-        println()
     }
 
     private fun bioDiversity(layout: Set<Cell>): Long {
@@ -249,5 +256,8 @@ class World(private val fileName: String) {
         }
     }
 }
+
+private fun Set<Cell>.minLayer() = this.minBy { it.layer }?.layer ?: 0
+private fun Set<Cell>.maxLayer() = this.maxBy { it.layer }?.layer ?: 0
 
 data class Cell(val x: Int, val y: Int, val layer: Int = 0)
